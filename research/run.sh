307585294466e714d2988c4a61a3d51df71582d9
Eric <tang8156@gmail.com>
2020-02-23 12:43:05 -0500

    added run.sh

--- research/run.sh
+++ research/run.sh
@@ -4,7 +4,7 @@ PIPELINE_CONFIG_PATH=/home/eric/models/research/ssd_mobilenet_v1_pets.config
 MODEL_DIR=/home/eric/models/research/model_out
 NUM_TRAIN_STEPS=1
 SAMPLE_1_OF_N_EVAL_EXAMPLES=1
-python object_detection/model_main.py \
+python3 object_detection/model_main.py \
     --pipeline_config_path=${PIPELINE_CONFIG_PATH} \
     --model_dir=${MODEL_DIR} \
     --num_train_steps=${NUM_TRAIN_STEPS} \
